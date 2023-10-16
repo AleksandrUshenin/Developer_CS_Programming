8448a11ac39b7c6c59bbd4e9a77b8843879952d5
Aleksandr Ushenin <sasha0625@mail.ru>
2023-10-16 20:19:52 +0300

    создание интерфейса BitsNumber

---
+++ С#Lessons/Lesson_3/BitsNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    internal interface BitsNumber
+    {
+        int SetBit(int value, int index, bool bit);
+        int GetBit(int value, int index);
+    }
+}
