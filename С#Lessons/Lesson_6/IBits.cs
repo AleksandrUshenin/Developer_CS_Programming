4b144953cad6fe71ddf4575b79b45243dcad118b
Aleksandr Ushenin <sasha0625@mail.ru>
2023-10-22 21:16:44 +0300

    add lesson 6

---
+++ С#Lessons/Lesson_6/IBits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    internal interface IBits
+    {
+        bool GetBit(int i);
+        void SetBit(bool bit, int index);
+    }
+}
