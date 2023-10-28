a71fc886cca888e4d19518b173048176812b68d9
Aleksandr Ushenin <sasha0625@mail.ru>
2023-10-28 18:51:56 +0300

    add calculator

---
+++ С#Lessons/Calculator/Logik/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Logik
+{
+    interface ICalculator
+    {
+        double Result { get;}
+        void Sum(double x);
+        void Sub(double x);
+        void Multy(double x);
+        void Divide(double x);
+        event EventHandler<EventArgs> MyEventHandler;
+        void Cansale();
+    }
+}
---
+++ С#Lessons/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Calculator.Logik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CalculatorClass calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Calculator";
+            calc = new CalculatorClass();
+            LabelResult.Content = calc.Result;
+            calc.MyEventHandler += Calc_MyEventHandler;
+        }
+        private void Calc_MyEventHandler(object? sender, EventArgs e)
+        {
+            if (sender is CalculatorClass)
+            {
+                LabelResult.Content = ((CalculatorClass)sender).Result;
+                TextBoxNumber.Text = "";
+            }
+            //Console.WriteLine(((CalculatorClass)sender).Result);
+        }
+        private double GetNum()
+        {
+            double res = 0;
+            double.TryParse(TextBoxNumber.Text, out res);
+            return res;
+        }
+        private void ButtonSum_Click(object sender, RoutedEventArgs e)
+        {
+            //int res = (int)LabelResult.Content;
+            calc.Sum(GetNum());
+        }
+
+        private void ButtonSub_Click(object sender, RoutedEventArgs e)
+        {
+            calc.Sub(GetNum());
+        }
+
+        private void ButtonMult_Click(object sender, RoutedEventArgs e)
+        {
+            calc.Multy(GetNum());
+        }
+
+        private void Cansel_Click(object sender, RoutedEventArgs e)
+        {
+            calc.Cansale();
+        }
+
+        private void ButtonDiv_Click(object sender, RoutedEventArgs e)
+        {
+            calc.Divide(GetNum());
+        }
+    }
+}
