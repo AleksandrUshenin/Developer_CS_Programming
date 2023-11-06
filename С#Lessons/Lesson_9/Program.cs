a27429353394d4ac3442a1cecb0be8b85c61d406
Aleksandr Ushenin <sasha0625@mail.ru>
2023-11-06 13:27:40 +0300

    add Lesson 9

---
+++ С#Lessons/Lesson_9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ex41_1(Ex41()));
+        }
+        static string[] Ex41()
+        {
+            Console.WriteLine("Введите числа :");
+            string? str = Console.ReadLine();
+            string[] numbers = str != null ? str.Split(',', ' ') : null;
+            return numbers;
+        }
+        static int Ex41_1(string[] numbers)
+        {
+            int count = 0;
+            foreach (string number in numbers)
+            {
+                int.TryParse(number, out int result);
+                if (result > 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
