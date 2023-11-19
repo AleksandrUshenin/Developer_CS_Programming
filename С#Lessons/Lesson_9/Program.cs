1b75a17eb47a2dcd3fd6d1110d27f0957c67f708
Aleksandr Ushenin <sasha0625@mail.ru>
2023-11-19 17:44:41 +0300

    update

--- С#Lessons/Lesson_9/Program.cs
+++ С#Lessons/Lesson_9/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Ex41_1(Ex41()));
+            //Console.WriteLine(Ex41_1(Ex41()));
+
+            int n = 3;
+            int m = 4;
+            int k = 2;
+
+            int ind = 1;
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < m; j++) 
+                {
+                    matrix[i, j] = ind;
+                    ind += k;
+                }
+            }
+
+            int n1 = matrix.GetLength(0);
+            int m1 = matrix.GetLength(1);
+            for (int i = 0; i < n1; i++)
+            {
+                for (int j = 0; j < m1; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int l1 = matrix.GetLength(0);
+            int l2 = matrix.GetLength(1);
+            double[] res = new double[l2];
+            for (int i = 0; i < l2; i++)
+            {
+                for (int j = 0; j < l1; j++)
+                {
+                    res[i] += matrix[j, i];
+                }
+                res[i] = res[i] / l1;
+            }
+
+            Console.WriteLine("The averages in columns are:");
+            for (int i = 0; i < res.Length; i++)
+            {
+                if (res[i] % 1 == 0)
+                {
+                    Console.Write(res[i] + ".00\t");
+                }
+                else
+                    Console.Write(res[i].ToString("#.##") + "\t");
+            }
         }
         static string[] Ex41()
         {
