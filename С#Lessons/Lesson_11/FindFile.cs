01fb4290152b00cccbe1ca12f6d81a91c2f66fe9
Aleksandr Ushenin <sasha0625@mail.ru>
2023-11-23 21:37:56 +0300

    add Урок 8. Потоки и буферизация

---
+++ С#Lessons/Lesson_11/FindFile.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    internal class FindFile : Stream
+    {
+        private FileInfo _file;
+
+        public byte[] Buffer { get; private set; }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return true; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return !_file.IsReadOnly; }
+        }
+
+        public override void Flush()
+        {
+            this._file = null;
+        }
+
+        private long _length;
+        public override long Length
+        {
+            get { return _length; }
+        }
+
+        private long _position;
+        public override long Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if (value < _file.Length && value >= -1)
+                    _position = value;
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int resRead = 0;
+            byte[] resultByte;
+            buffer = new byte[11];
+            using (FileStream fs = new FileStream(_file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                long len = fs.Length;
+                resultByte = new byte[len];
+                resRead = fs.Read(resultByte, offset, (int)len);
+                this.Buffer = resultByte;
+                return resRead;
+            }
+        }
+        public string ReadAll()
+        {
+            string res = "";
+            using (StreamReader sr = new StreamReader(_file.FullName, Encoding.UTF8))
+            {
+                res = sr.ReadToEnd();
+            }
+            return res;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            _length = value;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            using (FileStream fs = new FileStream(_file.FullName, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                fs.Write(buffer, offset, count);
+            }
+        }
+        public void Write(string text)
+        {
+            _Write(text, false);
+        }
+        public void Write(string text, bool append)
+        {
+            _Write(text, append);
+        }
+        private void _Write(string text, bool append)
+        {
+            using (StreamWriter sw = new StreamWriter(_file.FullName, append, Encoding.Default))
+            {
+                sw.Write(text);
+                //sw.WriteLine(text);
+            }
+        }
+
+        public FindFile(FileInfo file)
+        {
+            this._file = file;
+            this._position = 0;
+            this._length = _file.Length;
+        }
+        public FindFile(string file)
+        {
+            if (File.Exists(file))
+            {
+                FileInfo f = new FileInfo(file);
+                this._file = f;
+                this._position = 0;
+            }
+        }
+    }
+}
---
+++ С#Lessons/Lesson_11/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Lesson_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+                return;
+            if (args.Length == 2)
+            {
+                string dir = args[0];
+                string fileName = args[1];
+                Console.WriteLine(Find(dir, fileName));
+            }
+            else if (args.Length == 3)
+            {
+                Console.WriteLine(FindTextInFile(args[0], args[1], args[2]));
+            }
+        }
+        private static string Find(string dir, string nameFile)
+        {
+            if (!Directory.Exists(dir))
+                return "Такой директории не существует!";
+            DirectoryInfo dirInf = new DirectoryInfo(dir);
+            return RecursionFind(dirInf, nameFile);
+        }
+        private static string RecursionFind(DirectoryInfo dir, string nameFile)
+        {
+            var dirs = dir.GetDirectories();
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                if (file.Name.Equals(nameFile))
+                    return file.FullName;
+            }
+            foreach (var dirF in dirs)
+            {
+                string find = RecursionFind(dirF, nameFile);
+                if (find != null)
+                {
+                    return find;
+                }
+            }
+            return null;
+        }
+
+        private static string FindTextInFile(string dir, string extensoin, string text)
+        {
+            if (!Directory.Exists(dir))
+                return "Такой директории не существует!";
+            string res = FindTextFileExtension(new DirectoryInfo(dir), extensoin, text);
+            return res;
+        }
+        private static string FindTextFileExtension(DirectoryInfo dir, string extensoin, string text)
+        {
+            var files = dir.GetFiles("*" + extensoin);
+            string result = null;
+            foreach (var file in files)
+            {
+                using (FindFile ff = new FindFile(file))
+                {
+                    result = ff.ReadAll();
+                    if (result.Contains(text))
+                        return file.FullName;
+                }
+            }
+            return "Нету (";
+        }
+        private static string FindStringInFile(DirectoryInfo dir, string extensoin, string text)
+        {
+            var files = dir.GetFiles("*" + extensoin);
+            string result = null;
+            foreach (var file in files)
+            {
+                byte[] buff = new byte[10];
+                using (FindFile ff = new FindFile(@"G:\Metamit.com\Содержание.txt"))
+                {
+                    buff = new byte[10];
+                    int len = ff.Read(buff, 0, (int)ff.Length);
+                    buff = ff.Buffer;
+                    if (len == buff.Length)
+                    {
+                        result = Encoding.Default.GetString(buff);
+                        if (result.Contains(text))
+                            return file.FullName;
+                    }
+
+                }
+            }
+            return "Нету (";
+        }
+    }
+}
